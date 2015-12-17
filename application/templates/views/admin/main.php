6a7ec18dba330111234c51aa1ab7da60667699e2
rstuff <roma@nineseven.ru>
2015-12-17 11:24:54 +0300

    daasdasd

---
+++ application/templates/views/admin/main.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+
+    <link rel="icon" href="../img/content/favicon.ico" type="image/x-icon" />
+
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,700,900' rel='stylesheet' type='text/css'>
+    <link href="../../css/bootstrap.min.css" rel="stylesheet">
+    <script src="../../js/jQuery-2.1.3.min.js"></script>
+    <script src="../../js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="layout">
+</div><!-- END LAYOUT-->
+<?=$params['addition']?>
+
+<script src="../js/vendors/slick/slick.min.js"></script>
+<script src="../js/vendors/spin/spin.min.js"></script>
+<!--<script src="js/vendors/croppic/croppic.min.js"></script>-->
+<script src="../js/vendors/fancybox/jquery.fancybox.pack.js"></script>
+<script src="../js/interface.js"></script>
+<script src="../js/croppic.min.js"></script>
+<script src="../js/Share.js"></script>
+<script src="../js/antimat.js"></script>
+</body>
+</html>
